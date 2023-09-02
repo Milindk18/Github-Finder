556c0c5c61c70acf8b067c563a0460b6488ffa19
Milindk18 <coolboy.mk09@gmail.com>
2023-09-02 15:53:01 +0530

    changed theme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <div>
-        <h1 className='text-6xl mb-4 text-white'>GitHub Finder</h1>
-        <p className='mb-4 text-2xl font-light'>A React application to search GitHub profiles and view profile details.</p>
-        <p>Developed by: Anmol Pattnayak</p>
-        <p>Tech Stack used: React, Tailwind CSS and Daisy UI</p>
+      <h1 className="text-6xl mb-4 text-white">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React application to search GitHub profiles and view profile details.
+      </p>
+      <p>Developed by: Milind Kumar</p>
+      <p>Tech Stack used: React, Tailwind CSS and Daisy UI</p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
